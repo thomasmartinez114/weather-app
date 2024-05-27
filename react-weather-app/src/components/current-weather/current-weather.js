bbee4ab51cc0a357e9239de4d93e8028b770af64
Thomas Martinez <tmartinez114@gmail.com>
2024-05-27 19:45:43 -0400

    feels like/wind speed/humidity/pressure

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -22,19 +22,21 @@ const CurrentWeather = ({ data }) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">90°F</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°F
+            </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">9 mph</span>
+            <span className="parameter-value">{data.wind.speed} mph</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">33%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
