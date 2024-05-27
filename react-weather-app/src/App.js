c3c1cd7b87ea3382d1638ee22b8784a5e0f42a2a
Thomas Martinez <tmartinez114@gmail.com>
2024-05-27 17:52:40 -0400

    api is logging the data for the searched city // added fetch

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,11 +1,41 @@
+import React, { useState } from "react"
 import "./App.css"
 import Search from "./components/search/search"
 import CurrentWeather from "./components/current-weather/current-weather"
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api"
 
 function App() {
+  // weather hooks
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
+    const [lat, lon] = searchData.value.split(" ") // split the long lat into one values in array
+
+    // get current city's weather
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    )
+
+    // get current city's forecast
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    )
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json()
+        const forecastResponse = await response[1].json()
+
+        // set the useState hooks to their responses
+        setCurrentWeather({ city: searchData.label, ...weatherResponse })
+        setForecast({ city: searchData.label, ...forecastResponse })
+      })
+      .catch((err) => console.log(err))
   }
+
+  console.log(currentWeather)
+  console.log(forecast)
 
   return (
     <div className="container">
--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 }
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "40b3311f64d7597953ac91ae84b405cf"
