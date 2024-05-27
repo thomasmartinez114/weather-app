26e0a469b8481f3a82a9fd306ac3440972b0d1e9
Thomas Martinez <tmartinez114@gmail.com>
2024-05-26 21:58:48 -0400

    receiving cities long/lat and country code from the api search bar component

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "64cadd98e2msh4dbf562a4bbc531p1443ffjsne0932b548394",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+}
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
-import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../../api"
 
 const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
+  const [search, setSearch] = useState(null)
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            }
+          }),
+        }
+      })
+      .catch((err) => console.error(err))
+  }
 
   const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
 
   return (
     <AsyncPaginate
@@ -15,8 +35,9 @@ const Search = ({ onSearchChange }) => {
       debonuceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
