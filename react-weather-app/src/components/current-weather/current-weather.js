51dd29bf239c471305289caed5bce02f6b3753bc
Thomas Martinez <tmartinez114@gmail.com>
2024-05-26 22:51:48 -0400

    added more rows for the weather

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -5,10 +5,34 @@ const CurrentWeather = () => {
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
+          <p className="city">Providence</p>
           <p className="weather-description">Sunny</p>
         </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">85°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">90°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">9 mph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">33%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
       </div>
     </div>
   )
