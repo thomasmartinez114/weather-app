daad44efce0b5f5072316eb0f1b548047bfc138c
Thomas Martinez <tmartinez114@gmail.com>
2024-05-29 02:05:24 -0400

    day more details

--- react-weather-app/src/components/forecast/forecast.js
+++ react-weather-app/src/components/forecast/forecast.js
@@ -53,7 +53,34 @@ const Forecast = ({ data }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{day.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{day.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{day.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed</label>
+                  <label>{day.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level</label>
+                  <label>{day.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels Like:</label>
+                  <label>{Math.round(day.main.feels_like)}°F</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
