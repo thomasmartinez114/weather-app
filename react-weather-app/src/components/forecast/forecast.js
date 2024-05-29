3265dd23054eda720f279bab1b8f970ca9c4419e
Thomas Martinez <tmartinez114@gmail.com>
2024-05-28 21:16:48 -0400

    weather images for days

--- react-weather-app/src/components/forecast/forecast.js
+++ react-weather-app/src/components/forecast/forecast.js
@@ -11,10 +11,18 @@ const Forecast = ({ data }) => {
     <>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, idx) => (
+        {data.list.splice(0, 7).map((day, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
-              <AccordionItemButton>Hello</AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${day.weather[0].icon}.png`}
+                  />
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
