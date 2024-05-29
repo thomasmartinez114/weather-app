84b550d49b31fb2ecdce83e2c18bb7b333c3cf08
Thomas Martinez <tmartinez114@gmail.com>
2024-05-28 21:09:21 -0400

    implemented Accordion and rendered the data from api forecast spliced 0,7 for the days of week

--- react-weather-app/src/components/forecast/forecast.js
+++ react-weather-app/src/components/forecast/forecast.js
@@ -1,10 +1,25 @@
-import { Accordion } from "react-accessible-accordion"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion"
 
 const Forecast = ({ data }) => {
   return (
     <>
       <label className="title">Daily</label>
-      <Accordion allowZeroExpanded></Accordion>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Hello</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </>
   )
 }
