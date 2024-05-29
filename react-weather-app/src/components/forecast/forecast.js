dbcb9357d6cff00307cbbd34bfd1ac6a22ff1a26
Thomas Martinez <tmartinez114@gmail.com>
2024-05-28 21:22:27 -0400

    forecastDays created by using new Date().getDay() for todays day and slicing to end of array

--- react-weather-app/src/components/forecast/forecast.js
+++ react-weather-app/src/components/forecast/forecast.js
@@ -6,7 +6,27 @@ import {
   AccordionItemButton,
 } from "react-accessible-accordion"
 
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+]
+
 const Forecast = ({ data }) => {
+  // get the day it is today
+  const dayInAWeek = new Date().getDay()
+
+  // use slice (does not mutate original arr), add the sliced days to end of array
+  // ex.) Wednesday is today, slice Mon-Tue add to end of arr
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  )
+  //   console.log(forecastDays)
+
   return (
     <>
       <label className="title">Daily</label>
@@ -21,6 +41,7 @@ const Forecast = ({ data }) => {
                     className="icon-small"
                     src={`icons/${day.weather[0].icon}.png`}
                   />
+                  <label className="day"></label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
